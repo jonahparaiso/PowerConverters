bf1be3104b857fd0a58b9afe882a5c95d9335f81
Jonah Paraiso <jonah.paraiso@gmail.com>
2020-08-13 01:09:14 -0700

    big commit to add missing files

--- app/src/test/java/com/bibfortuna/powerconverters/PlayerTest.java
+++ app/src/test/java/com/bibfortuna/powerconverters/PlayerTest.java
@@ -12,14 +12,14 @@ public class PlayerTest {
 //        tester.levelUp(new Scoundrel(), 7, true);
 //        tester.levelUp(new Noble(), 6, true);
 
-//        Player jonah = new Player(new Scoundrel(), 13, new JediGuardian(), 7, true, "feats");
-//        jonah.printSkills();
-//        jonah.printAttributes();
-//        jonah.printSavingThrows();
-//        jonah.printMisc();
-//        jonah.printHealth();
-//        jonah.printCombat();
-
+        Player jonah = new Player(new Scoundrel(), 13, new JediGuardian(), 7, true, "list");
+        jonah.printSkills();
+        jonah.printAttributes();
+        jonah.printSavingThrows();
+        jonah.printMisc();
+        jonah.printHealth();
+        jonah.printCombat();
+//
 //        Player yolo = new Player();
 //        yolo.printSkills();
 //        yolo.printAttributes();
@@ -32,15 +32,15 @@ public class PlayerTest {
 //        Player multi = new Player(new TechSpecialist(), 10, new Scoundrel(), 2, true, "list");
     }
 
-    @Test
-    public void printConstructor3() {
-//        System.out.println("Testing printConstructor3");
-        Player gunthar = new Player(Species.HUMAN, new Thug(), 2, false, "list");
-        gunthar.printSkills();
-        gunthar.printAttributes();
-        gunthar.printCombat();
-        gunthar.printHealth();
-        gunthar.printMisc();
-        gunthar.printSavingThrows();
-    }
+//    @Test
+//    public void printConstructor3() {
+////        System.out.println("Testing printConstructor3");
+//        Player gunthar = new Player(Species.HUMAN, new Thug(), 2, false, "list");
+//        gunthar.printSkills();
+//        gunthar.printAttributes();
+//        gunthar.printCombat();
+//        gunthar.printHealth();
+//        gunthar.printMisc();
+//        gunthar.printSavingThrows();
+//    }
 }
